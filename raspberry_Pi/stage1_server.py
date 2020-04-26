cf13651daa53ea544928b53b4b0f9bee9aa41a16
Yuqi Chen <59162505+yuqiChen94@users.noreply.github.com>
2020-04-26 23:21:19 +0800

    Add files via upload

---
+++ raspberry_Pi/stage1_server.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import zmq
+import numpy as np
+import time
+
+'''set tcp address'''
+ip = "10.0.1.231"
+port = "5510"
+tcp_address = "tcp://" + ip + ":" + port
+
+
+context = zmq.Context()
+socket = context.socket(zmq.PUB)
+socket.bind(tcp_address)
+
+
+while True:
+    '''send old vector to laptop'''
+    vo = np.loadtxt('stage1old.txt')
+    socket.send_string("%s" % vo)
+    time.sleep(1)
